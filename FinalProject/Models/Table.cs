1e941fe4103bfbba6df057a55117c1bf27fbc32f
Oleh <ehminvoitsek@gmail.com>
2026-09-12 20:46:23 +0300

    Add parameterless constructor to Table class
    Added a parameterless constructor to the Table class.

--- FinalProject/Models/Table.cs
+++ FinalProject/Models/Table.cs
@@ -1,4 +1,4 @@
-﻿namespace FinalProject.Models
+namespace FinalProject.Models
 {
     public class Table
     {
@@ -13,6 +13,10 @@
         public string Material { get; set; }
         public decimal Price { get; set; }
 
+        public Table()
+        {
+
+        }
 
     }
 }
