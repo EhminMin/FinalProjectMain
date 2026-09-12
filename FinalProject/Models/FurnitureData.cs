1d07fdc2a0c3e8c6e91e6b61c37f0df6609ed51d
Oleh <ehminvoitsek@gmail.com>
2026-09-12 20:45:54 +0300

    Add constructor to FurnitureData class

--- FinalProject/Models/FurnitureData.cs
+++ FinalProject/Models/FurnitureData.cs
@@ -1,8 +1,13 @@
-﻿namespace FinalProject.Models
+namespace FinalProject.Models
 {
     public class FurnitureData
     {
         public List<Table> Tables { get; set; } = new();
         public List<Chair> Chairs { get; set; } = new();
+
+        public FurnitureData()
+        {
+
+        }
     }
 }
